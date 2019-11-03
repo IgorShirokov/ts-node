df874d5fc54648413b175ae61c7f9046b7dc7275
Igor Shirokov <shirokov-iv@mail.ru>
2019-11-03 17:53:46 +0500

    parsing form bodies

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,25 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('hi there');
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input type="email" name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" name="password" />
+      </div>
+      <submit>Submit</submit>
+    </form>
+  `);
+});
+
+router.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
 });
 
 export { router };
