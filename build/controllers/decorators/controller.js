5f8662b306df91645f0c8c298405baaf64d1ed4f
Igor Shirokov <shirokov-iv@mail.ru>
2019-11-04 13:50:13 +0500

    automated validation

--- build/controllers/decorators/controller.js
+++ build/controllers/decorators/controller.js
@@ -10,7 +10,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 var metadatakeys_1 = require("./metadatakeys");
 var AppRouter_1 = require("../../AppRouter");
-var bodyValidators = function (keys) { return function (req, res, next) { }; };
+var bodyValidators = function (keys) { return function (req, res, next) {
+    if (!req.body) {
+        res.status(422).send('Invalid request');
+    }
+    for (var _i = 0, keys_1 = keys; _i < keys_1.length; _i++) {
+        var key = keys_1[_i];
+        if (!req.body[key]) {
+            res.status(422).send('Invalid request');
+            return;
+        }
+    }
+    next();
+}; };
 exports.Controller = function (routePrefix) { return function (target) {
     for (var key in target.prototype) {
         var router = AppRouter_1.AppRouter.getInstance();
@@ -18,8 +30,11 @@ exports.Controller = function (routePrefix) { return function (target) {
         var path = Reflect.getMetadata(metadatakeys_1.MetadataKeys.path, target.prototype, key);
         var method = Reflect.getMetadata(metadatakeys_1.MetadataKeys.method, target.prototype, key);
         var middlewares = Reflect.getMetadata(metadatakeys_1.MetadataKeys.middleware, target.prototype, key) || [];
+        var requiredBodyProps = Reflect.getMetadata(metadatakeys_1.MetadataKeys.validator, target.prototype, key) || [];
+        var validator = bodyValidators(requiredBodyProps);
         if (path) {
-            router[method].apply(router, __spreadArrays(["" + routePrefix + path], middlewares, [routeHandler]));
+            router[method].apply(router, __spreadArrays(["" + routePrefix + path], middlewares, [validator,
+                routeHandler]));
         }
     }
 }; };
--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -9,7 +9,18 @@ const bodyValidators = (keys: string): RequestHandler => (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  if (!req.body) {
+    res.status(422).send('Invalid request');
+  }
+  for (let key of keys) {
+    if (!req.body[key]) {
+      res.status(422).send('Invalid request');
+      return;
+    }
+  }
+  next();
+};
 
 export const Controller = (routePrefix: string) => (target: Function) => {
   for (let key in target.prototype) {
@@ -24,8 +35,18 @@ export const Controller = (routePrefix: string) => (target: Function) => {
     const middlewares =
       Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) || [];
 
+    const requiredBodyProps =
+      Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) || [];
+
+    const validator = bodyValidators(requiredBodyProps);
+
     if (path) {
-      router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+      router[method](
+        `${routePrefix}${path}`,
+        ...middlewares,
+        validator,
+        routeHandler
+      );
     }
   }
 };
